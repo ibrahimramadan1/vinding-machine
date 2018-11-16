98e4891d0faf456ca603cc7d9a1cccf9960db3c6
AhmedElsayed23 <36089631+AhmedElsayed23@users.noreply.github.com>
2018-11-16 07:48:42 -1100

    new added

--- stock.cpp
+++ stock.cpp
@@ -1,1 +1,27 @@
 
+#include "stock.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+Stock::Stock() // empty constructor.
+{
+    //ctor
+}
+
+ostream& operator<< (ostream& out,Stock a) //function to print the menu.
+{
+    cout << setw(14) << "Item "
+         << setw(8)  << "Cost "
+         << setw(16) << "Amount\n";
+    cout << "---------------------------------------\n";
+
+    for (int i = 0 ; i < 10 ; i++)
+    {
+        out << setw (11) << a.Item[i];
+        out << setw (8)  << a.price[i]  << " LE";
+        out << setw (11) << a.Amount[i] << endl;
+    }
+    cout << "----------------------------------------\n";
+}
